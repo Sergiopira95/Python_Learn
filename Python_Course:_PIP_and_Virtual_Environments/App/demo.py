a7fdd7d5e9901cd1a07a3645438854311c58c862
Sergio Andrés Piratoba Forero <54446393+Sergiopira95@users.noreply.github.com>
2024-04-12 19:34:01 -0500

    Delete Python_Course:_PIP_and_Virtual_Environments directory

--- Python_Course:_PIP_and_Virtual_Environments/App/demo.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-def read_csv(path):
- with open(path, 'r') as csvfile:
-   reader = csv.reader(csvfile, delimiter=',')
-   for row in reader:
-      print(row)
-
-read_csv('./app/data.csv')
--- Python_Course:_PIP_and_Virtual_Environments/README.md
+++
@@ -1,8 +0,0 @@
-# Game Project
-
-Para correr el juego debes seguir las siguientes instrucciones en la terminal:
-
-```sh
-cd RPS_Game
-python3 main.py
-```
